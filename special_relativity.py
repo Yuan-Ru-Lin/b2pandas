6b9363c0b51c4eea504ec793fa3f98d0f998df0b
Leon_Lin <LeonLin1997@gmail.com>
2019-04-03 14:58:04 +0800

    Format document

--- special_relativity.py
+++ special_relativity.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import numpy as np
 
+
 class Vector3D:
-    
+
     def __init__(self, c1, c2, c3):
         self.c1 = c1
         self.c2 = c2
         self.c3 = c3
-    
+
     def Magnitude(self):
         return np.sqrt(self.c1 ** 2 + self.c2 ** 2 + self.c3 ** 2)
-    
+
     def __mul__(self, other):
         if isinstance(other, Vector3D):
             return self.c1 * other.c1 + self.c2 * other.c2 + self.c3 * other.c3
         else:
             return Vector3D(self.c1 * other, self.c2 * other, self.c3 * other)
-    
+
     def __rmul(self, other):
         return self.__mul__(other)
-    
+
     def __truediv__(self, divisor: float):
         return Vector3D(self.c1 / divisor, self.c2 / divisor, self.c3 / divisor)
-    
+
     def __str__(self):
         return "x:\n{0},\ny:\n{1},\nz:\n{2}".format(self.c1, self.c2, self.c3)
 
 
 class FourMomentum:
-    
+
     def __init__(self, c0, c1, c2, c3):
         self.c0 = c0
         self.c1 = c1
@@ -38,28 +39,28 @@ class FourMomentum:
     @classmethod
     def from3Momentum(cls, energy, threeMomentum: Vector3D):
         return cls(c0=energy, c1=threeMomentum.c1, c2=threeMomentum.c2, c3=threeMomentum.c3)
-    
+
     def __add__(self, other):
         return FourMomentum(self.c0 + other.c0, self.c1 + other.c1, self.c2 + other.c2, self.c3 + other.c3)
-    
+
     def __mul__(self, other):
         return - self.c0 * other.c0 + self.c1 * other.c1 + self.c2 * other.c2 + self.c3 * other.c3
-    
+
     def temporalPart(self):
         return self.c0
-    
+
     def spatialPart(self):
         return Vector3D(self.c1, self.c2, self.c3)
-    
+
     def invariantMass(self):
-        return np.sqrt(-1 * (self * self)) 
-    
+        return np.sqrt(-1 * (self * self))
+
     def _LorentzFactor(self):
         return self.c0 / self.invariantMass()
-    
+
     def _beta3D(self):
         return Vector3D(self.c1, self.c2, self.c3) / (self.invariantMass() * self._LorentzFactor())
-    
+
     def boostedTo(self, targetedVelocity: Vector3D):
         b = targetedVelocity
         r = 1 / np.sqrt(1 - b.Magnitude()**2)
@@ -70,7 +71,7 @@ class FourMomentum:
               + (r-1) * b.c3 * b.c1 / b.Magnitude()**2 * self.c3)
         c2 = (- r * b.c2 * self.c0
               + (r-1) * b.c1 * b.c2 / b.Magnitude()**2 * self.c1
-              + (1 + (r-1) * b.c2 * b.c2 / b.Magnitude()**2) * self.c2 
+              + (1 + (r-1) * b.c2 * b.c2 / b.Magnitude()**2) * self.c2
               + (r-1) * b.c3 * b.c2 / b.Magnitude()**2 * self.c3)
         c3 = (- r * b.c3 * self.c0
               + (r-1) * b.c1 * b.c3 / b.Magnitude()**2 * self.c1
--- tests/test_Vector3D.py
+++ tests/test_Vector3D.py
@@ -1,9 +1,11 @@
+import unittest
 import sys
 sys.path.append("../")
 from special_relativity import *
 
+
 class TestVector3D(unittest.TestCase):
-    
+
     def setUp(self):
         self.df = pd.DataFrame([[1, 2, 3, 4],
                                 [2, 4, 5, 6],
@@ -18,15 +20,21 @@ class TestVector3D(unittest.TestCase):
 
     def test__mul__(self):
         # Verify the multiplication in Vector3D * Vector3D (= Number)
-        np.testing.assert_allclose(self.x * self.x, self.df[0]**2 + self.df[1]**2 + self.df[2]**2)
-        
+        np.testing.assert_allclose(
+            self.x * self.x, self.df[0]**2 + self.df[1]**2 + self.df[2]**2)
+
         # Verify the multiplication in Vector3D * Number (= Vector3D)
-        self.assertTrue(self.x * self.y == Vector3D(self.df[0] * self.df[3], self.df[1] * self.df[3], self.df[2]* self.df[3]))
-        self.assertTrue(self.x.__mul__(self.y) == Vector3D(self.df[0] * self.df[3], self.df[1] * self.df[3], self.df[2]* self.df[3]))
-        
+        self.assertTrue(self.x * self.y == Vector3D(
+            self.df[0] * self.df[3], self.df[1] * self.df[3], self.df[2] * self.df[3]))
+        self.assertTrue(self.x.__mul__(self.y) == Vector3D(
+            self.df[0] * self.df[3], self.df[1] * self.df[3], self.df[2] * self.df[3]))
+
     def test__rmul__(self):
         pass
         # Verify the multiplication in Number * Vector3D (= Vector3D)
 #         import pdb; pdb.set_trace()
 #         self.assertTrue(self.y * self.x == Vector3D(self.df[0] * self.df[3], self.df[1] * self.df[3], self.df[2]* self.df[3]))
 
+
+if __name__ == "__main__":
+    unittest.main()
