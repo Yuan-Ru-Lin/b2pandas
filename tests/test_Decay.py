b1fca47942ccdad45257020226617808d2775870
Leon_Lin <LeonLin1997@gmail.com>
2020-01-21 13:36:19 +0800

    Correct the test for add_variables()

--- tests/test_Decay.py
+++ tests/test_Decay.py
@@ -8,22 +8,33 @@ class TestDecay(unittest.TestCase):
         self.lambda0 = Node(symbol="Lambda0", name="Lambda0")
         self.proton = Node(symbol="p", name="proton")
         self.pion = Node(symbol="pi-", name="pion")
-        self.commonVariables = ['M', 'PDG', 'mcPDG', 'isSignal', ]
+        self.commonVariables = ['mcPDG', 'isSignal']
         self.my_decay = Decay(self.lambda0)
-
-    def test_add_daughter(self):
         self.lambda0.add_daughter(self.proton)
         self.lambda0.add_daughter(self.pion)
+
+    def test_add_daughter(self):
+        """Check if the daughters has been linked to the mother mutually"""
         self.assertEqual(self.lambda0.daughters[0], self.proton)
         self.assertEqual(self.lambda0.daughters[1], self.pion)
+        self.assertEqual(self.proton.mother, self.lambda0)
+        self.assertEqual(self.pion.mother, self.lambda0)
 
     def test_add_variables(self):
-        self.lambda0.add_variables(['nTracks', 'dr', 'chiProb',
-                                    'cosAngleBetweenMomentumAndVertexVector', ] + self.commonVariables)
-        self.proton.add_variables(['protonID', 'charge', 'clusterE', 'clusterHighestE',
-                                   'clusterE1E9', 'clusterE9E21', 'clusterNHits', ] + self.commonVariables)
-        self.pion.add_variables(['pionID', 'charge', ] + self.commonVariables)
-        print(self.my_decay.translate_variables_for_BASF2())
+        self.lambda0.add_variables(['nTracks', 'dr'] + self.commonVariables)
+        self.proton.add_variables(['protonID', 'charge'] + self.commonVariables)
+        self.pion.add_variables(['pionID', 'charge'] + self.commonVariables)
+
+        # Check if the variables has been assigned
+        self.assertEqual(self.lambda0.variables, ['nTracks', 'dr', 'mcPDG', 'isSignal'])
+        self.assertEqual(self.proton.variables, ['protonID', 'charge', 'mcPDG', 'isSignal'])
+        self.assertEqual(self.pion.variables, ['pionID', 'charge', 'mcPDG', 'isSignal'])
+        self.assertEqual(self.my_decay.translate_variables_for_BASF2(),
+                         ['nTracks', 'dr', 'mcPDG', 'isSignal',
+                          'daughter(0, protonID)', 'daughter(0, charge)',
+                          'daughter(0, mcPDG)', 'daughter(0, isSignal)',
+                          'daughter(1, pionID)', 'daughter(1, charge)',
+                          'daughter(1, mcPDG)', 'daughter(1, isSignal)'])
 
     def tearDown(self):
         self.lambda0 = None
