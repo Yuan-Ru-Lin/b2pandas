53d1755f4388962658f2e5170a94578637b379c7
Leon_Lin <LeonLin1997@gmail.com>
2020-02-03 22:18:07 +0800

    Rewrite Node and Decay as Particle, an extension of NodeMixin
    See also: https://anytree.readthedocs.io/en/latest/api/anytree.node.html#anytree.node.nodemixin.NodeMixin

--- steering_helper.py
+++ steering_helper.py
@@ -1,87 +1,40 @@
-class Node():
+from anytree import NodeMixin, LevelOrderGroupIter
 
-    def __init__(self, symbol, name):
-        self.symbol = symbol
+class BaseParticle:
+
+    def __init__(self, name, variables):
         self.name = name
-        self.mother = None
-        self.daughters = []
-        self.variables = []
+        self.variables = variables
 
-    def __str__(self):
-        return f'{self.name} ({self.symbol}) -> {self.daughters}; variables: {self.variables}'
+class Particle(BaseParticle, NodeMixin):
 
-    def __repr__(self):
-        particle_string = f'{self.name} ({self.symbol})'
-        if self.daughters:
-            particle_string += f' -> {self.daughters}'
-        return particle_string
+    def __init__(self, name, variables, parent=None, children=None):
+        super(Particle, self).__init__(name=name, variables=variables)
+        self.parent = parent
+        if children:
+            self.children = children
 
-    def add_variables(self, variables):
-        self.variables += variables
+    def getVariablePrefixed(self, variable):
+        node = self
+        string = variable
+        while node.parent:
+            string = Particle.daughter(string, node.parent.children.index(node))
+            node = node.parent
+        return string
 
-    def add_daughter(self, daughter_node):
-        self.daughters.append(daughter_node)
-        self.daughters[-1].mother = self
+    def getVariablesPrefixed(self):
+        return [self.getVariablePrefixed(variable) for variable in self.variables]
 
-    def rank(self):
-        assert self.mother, "This node has no mother!"
-        return self.mother.daughters.index(self)
+    def getAncestorsVariablesPrefixed(self):
+        lst = []
+        for children in LevelOrderGroupIter(self):
+            for node in children:
+                lst += node.getVariablesPrefixed()
+        return lst
 
-    def own(self, variable):
-        if self.mother:
-            daughter_order = self.rank()
-            return variable[:21] == "daughter__bo" + str(daughter_order) + "__cm__sp"
-        else:
-            return variable in self.variables
-
-    def distribute_variables_to_daughters(self):
-        for daughter in self.daughters:
-            for variable in self.variables:
-                if daughter.own(variable):
-                    daughter.variables.append(variable)
-                    self.variables.remove(variable)
-
-    def rename_every_own_variables(self):
-        renamed_variables = []
-        for variable in self.variables:
-            renamed_variables.append(no_parentheses(variable))
-        self.variables = renamed_variables
-
-
-class Decay():
-
-    def __init__(self, mother: Node):
-        self.mother = mother
-
-    def rename_every(self, variables):
-        self.mother.variables = variables
-        self.mother.distribute_variables_to_daughters()
-        self.mother.rename_every_own_variables()
-        for daughter in self.mother.daughters:
-            daughter.rename_every_own_variables()
-
-    def translate_variables_for_BASF2(self):
-        basf2_variables = []
-        basf2_variables += self.mother.variables
-        for daughter in self.mother.daughters:
-            basf2_variables += map(lambda var: 'daughter(' +
-                                   str(daughter.rank()) + ', ' + var + ')', daughter.variables)
-        return basf2_variables
-
-    def get_variables(self):
-        variables_to_print = []
-        variables_to_print += self.mother.variables
-        for daughter in self.mother.daughters:
-            variables_to_print += map(lambda var: daughter.name +
-                                      "_" + var, daughter.variables)
-        return variables_to_print
-
-
-def no_parentheses(var):
-    if "daughter__bo" in var:
-        return var[21:-4]
-    else:
-        return var
+    @staticmethod
+    def daughter(variable, order):
+        return f"daughter({order}, {variable})"
 
 
 fourMomentum = ['E', 'px', 'py', 'pz']
--- tests/test_Decay.py
+++ tests/test_Decay.py
@@ -1,35 +1,26 @@
 import unittest
-from ..steering_helper import Decay, Node
+from ..steering_helper import Particle
 
 
-class TestDecay(unittest.TestCase):
+class TestParticle(unittest.TestCase):
 
     def setUp(self):
-        self.lambda0 = Node(symbol="Lambda0", name="Lambda0")
-        self.proton = Node(symbol="p", name="proton")
-        self.pion = Node(symbol="pi-", name="pion")
         self.commonVariables = ['mcPDG', 'isSignal']
-        self.my_decay = Decay(self.lambda0)
-        self.lambda0.add_daughter(self.proton)
-        self.lambda0.add_daughter(self.pion)
+        self.lambda0 = Particle(name="lambda0",
+            variables=['nTracks', 'dr'] + self.commonVariables)
+        self.proton = Particle(name="proton",
+            variables=['protonID', 'charge'] + self.commonVariables,
+            parent=self.lambda0)
+        self.pion = Particle(name="pion",
+            variables=['pionID', 'charge'] + self.commonVariables,
+            parent=self.lambda0)
 
-    def test_add_daughter(self):
-        """Check if the daughters has been linked to the mother mutually"""
-        self.assertEqual(self.lambda0.daughters[0], self.proton)
-        self.assertEqual(self.lambda0.daughters[1], self.pion)
-        self.assertEqual(self.proton.mother, self.lambda0)
-        self.assertEqual(self.pion.mother, self.lambda0)
-
-    def test_add_variables(self):
+    def test_getVariablesPrefixed(self):
         """Check if the variables has been assigned"""
-        self.lambda0.add_variables(['nTracks', 'dr'] + self.commonVariables)
-        self.proton.add_variables(['protonID', 'charge'] + self.commonVariables)
-        self.pion.add_variables(['pionID', 'charge'] + self.commonVariables)
-
         self.assertEqual(self.lambda0.variables, ['nTracks', 'dr', 'mcPDG', 'isSignal'])
         self.assertEqual(self.proton.variables, ['protonID', 'charge', 'mcPDG', 'isSignal'])
         self.assertEqual(self.pion.variables, ['pionID', 'charge', 'mcPDG', 'isSignal'])
-        self.assertEqual(self.my_decay.translate_variables_for_BASF2(),
+        self.assertEqual(self.lambda0.getAncestorsVariablesPrefixed(),
                          ['nTracks', 'dr', 'mcPDG', 'isSignal',
                           'daughter(0, protonID)', 'daughter(0, charge)',
                           'daughter(0, mcPDG)', 'daughter(0, isSignal)',
@@ -41,7 +32,6 @@ class TestDecay(unittest.TestCase):
         self.proton = None
         self.pion = None
         self.commonVariables = None
-        self.my_decay = None
 
 if __name__ == '__main__':
     unittest.main()
